4eae6178042a5ba2c9d1b63589d680c2db5daf09
JoseAntonio2100 <bautistacuevas2100@gmail.com>
2025-03-25 18:19:01 -0600

    akjdawda

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, NavbarComponent],
+  templateUrl: './app.component.html',  
+
 })
 export class AppComponent {
   title = 'econetfront';
---
+++ src/app/componentes/navbar/navbar.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {  RouterOutlet } from '@angular/router';
+import { itemNavbar } from '../../interfaces/interface';
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterOutlet],
+  templateUrl: './navbar.component.html',
+  styles:[]
+})
+export class NavbarComponent {
+  listaMenu: itemNavbar[]=[
+    {
+      title: 'login',
+      url: '/login'
+    },
+    {
+      title: 'register',
+      url:'/register'
+    }
+  ]
+
+}
