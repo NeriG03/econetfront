f094286a1ead7cbbc8a3c9ff365fab6d06c3977b
DiegoSosaRam <143459270+DiegoSosaRam@users.noreply.github.com>
2025-04-02 18:56:34 -0600

    detalles de las paginas
    v1.0.0 beta

--- src/app/pages/cuidemos/cuidemos.component.ts
+++ src/app/pages/cuidemos/cuidemos.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cuidemos',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './cuidemos.component.html',
   styles: ''
 })
--- src/app/pages/noticias/noticias.component.ts
+++ src/app/pages/noticias/noticias.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-noticias',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './noticias.component.html',
   styles: ''
 })
