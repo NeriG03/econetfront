296182a3ac85036a413ac1dfdd753cb1025dac5b
Vicente Barrera <vic.bar1@outlook.com>
2025-05-21 16:36:15 -0600

    Rediseñode noticias

--- src/app/pages/noticias/noticias.component.ts
+++ src/app/pages/noticias/noticias.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-noticias',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './noticias.component.html',
   styles: ''
 })
